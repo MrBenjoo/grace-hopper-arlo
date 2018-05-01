e8b650319a36677dcfdc57631a23b89d61620ed5
Filip Weilid <filip.weilid@gmail.com>
2018-05-01 14:45:59 +0200

    Revert "PWM funkar-ish"
    This reverts commit dc17cc6c469020d94c52de79731b4f9472d34262.

--- GccBoardProject1/GccBoardProject1/Debug/src/motorController.d
+++ GccBoardProject1/GccBoardProject1/Debug/src/motorController.d
@@ -134,7 +134,7 @@ src/motorController.d src/motorController.o: ../src/motorController.c \
  ../src/ASF/sam/drivers/pio/pio.h \
  ../src/ASF/common/services/ioport/ioport.h \
  ../src/ASF/common/services/ioport/sam/ioport_pio.h \
- ../src/ASF/sam/drivers/pmc/sleep.h ../src/ASF/sam/drivers/pwm/pwm.h \
+ ../src/ASF/sam/drivers/pmc/sleep.h \
  ../src/ASF/sam/drivers/pio/pio_handler.h
 
 ../src/asf.h:
@@ -414,7 +414,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-no
 ../src/ASF/common/services/ioport/sam/ioport_pio.h:
 
 ../src/ASF/sam/drivers/pmc/sleep.h:
-
-../src/ASF/sam/drivers/pwm/pwm.h:
 
 ../src/ASF/sam/drivers/pio/pio_handler.h:
