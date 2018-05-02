a40c72466b7cd38c8a789c0d77af350444732e58
unknown <sebastian@email.com>
2018-05-02 15:52:11 +0200

    V.1.2 av sammanställda kod, nu med PWM. ATT FIXA: 1 av 4 intterupts triggas

---
+++ Sammanstäld kod Atmel/v.1.2/FreeRTOS/src/initializations/pins_init.h
@@ -0,0 +1,25 @@
+/*
+	This header file can be used instead of IOPORT to get access to the digital pins. It will be easier to use
+	because you do not to check the pin mappings all the time.
+*/
+#ifndef PINSINIT_H_
+#define PINSINIT_H_
+
+/* digital pins 2-13 */
+#define pin13 PIO_PB27_IDX
+#define pin12 PIO_PD8_IDX
+#define pin11 PIO_PD7_IDX
+#define pin10 PIO_PA28_IDX
+#define pin9  PIO_PC21_IDX
+#define pin8  PIO_PC22_IDX
+#define pin7  PIO_PC23_IDX
+#define pin6  PIO_PC24_IDX
+#define pin5  PIO_PC25_IDX
+#define pin4  PIO_PC26_IDX
+#define pin3  PIO_PC28_IDX
+#define pin2  PIO_PB25_IDX
+#define pin5i  PIO_PC25
+#define pin7i  PIO_PC23
+#define pin8i  PIO_PC22
+#define pin10i PIO_PA28
+#endif /* PINSINIT_H_ */
---
+++ Sammanstäld kod Atmel/v.1.2/FreeRTOS/src/tasks/task1/task1.c
@@ -0,0 +1,50 @@
+/* 
+	The code in this file was used to test FreeRTOS. 
+	Change the code for your purpose.
+*/
+
+#include <asf.h>
+#include "task1.h"
+#include "../initializations/pins_init.h"
+#include "Reglering/Reglering.h"
+#include "../motorController.h"
+#include "../encoderISR.h"
+//#define xTimeT1 50
+
+/*
+* Function: task1
+* -------------------
+* Performs an action every xTimeT1 milliseconds
+*/
+void task1(void *pvParamters)
+{
+	//portTickType xLastWakeTime;
+	//portTickType xTimeIncrement = xTimeT1/portTICK_RATE_MS; /* set ticks (xTimeT1) in milliseconds */
+	//xLastWakeTime = xTaskGetTickCount();					/* the count of ticks since vTaskStartScheduler was called */
+	
+		//vTaskDelayUntil(&xLastWakeTime, xTimeIncrement); /* constant period, independent of execution time */
+		//vTaskDelay(xTimeT1);								 /* no constant period, dependent of execution time */ 
+		
+		//int ticksContinously = 0;
+		//int ticksPassed = 0;
+		//int ticksReference = 0;
+	drive(30, 30, 1, 1); //uses pin35 and pin42
+
+	while(1){
+		drive(30, 30, 1, 1); //uses pin35 and pin42
+		printf("Test\n");
+		getRightBPulseTime();
+		getLeftAPulseTime();
+		getRightPulses();
+		getLeftPulses();
+	//ticksReference = xTaskGetTickCount();
+	}
+	/*while(ticksPassed<3000){
+		ticksPassed =  xTaskGetTickCount() - ticksReference;
+	}
+	drive(0, 0, 1, 1);
+	getRightPulses();
+	getLeftPulses();
+	*/	
+	}
+
