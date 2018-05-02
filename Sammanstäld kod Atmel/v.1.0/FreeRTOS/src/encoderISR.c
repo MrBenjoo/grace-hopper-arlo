0f79babeba9ccbfec9f0cba45b344dadb4f00c77
unknown <sebastian@email.com>
2018-05-02 11:22:59 +0200

    Ny mapp för sammanstäld kod + Uppdaterad kod v.1.1

---
+++ Sammanstäld kod Atmel/v.1.0/FreeRTOS/src/encoderISR.c
@@ -0,0 +1,195 @@
+/*
+ * encoderISR.c
+ *
+ * Created: 2018-04-20 11:13:29
+ *  Author: Gustav,Sebastian
+ */ 
+#include <asf.h>
+#include <stdio.h>
+#include "encoderISR.h"
+#include "../initializations/pins_init.h"
+
+void encoder_init(){ //initialise the encoders
+	lAPrevTick=0; 
+	rAPrevTick=0;
+	lACurrentTick=0;
+	rACurrentTick=0;
+	lBPrevTick=0;
+	rBPrevTick=0;
+	lBCurrentTick=0;
+	rBCurrentTick=0;
+	lAPulses=0;
+	lAPulseTime=0;
+	rAPulses=0;
+	rAPulseTime=0;
+	lBPulses=0;
+	lBPulseTime=0;
+	rBPulses=0;
+	rBPulseTime=0;
+	msPerTick=1; // Time for a tick is 1 ms on SAM3X8E CPU
+	pmc_enable_periph_clk(ID_PIOC);
+
+/* Initializes interrupt pin for the left encoder A, trigger on rise edge, pin5*/
+pio_set_input(PIOC,pin5i,NULL);
+pio_handler_set(PIOC, ID_PIOC,pin5i, PIO_IT_RISE_EDGE , leftEncoderAISR);
+pio_enable_interrupt(PIOC,pin5i); //digital pin 5
+
+/* Initializes interrupt pin for the right encoder A, trigger on rise edge, pin7*/
+pio_set_input(PIOC,pin7i,NULL);
+pio_handler_set(PIOC,ID_PIOC,pin7i,PIO_IT_RISE_EDGE,rightEncoderAISR);
+pio_enable_interrupt(PIOC,pin7i); //digital pin 7
+
+/* Initializes interrupt pin for the left encoder B, trigger on rise edge, pin8*/
+pio_set_input(PIOC,pin8i,NULL);
+pio_handler_set(PIOC, ID_PIOC,pin8i, PIO_IT_RISE_EDGE , leftEncoderBISR);
+pio_enable_interrupt(PIOC,pin8i); //digital pin 8
+
+/* Initializes interrupt pin for the right encoder B, trigger on rise edge, pin10*/
+pio_set_input(PIOC,pin10i,NULL);
+pio_handler_set(PIOC,ID_PIOC,pin10i,PIO_IT_RISE_EDGE,rightEncoderBISR);
+pio_enable_interrupt(PIOC,pin10i); //digital pin 10
+NVIC_EnableIRQ(PIOC_IRQn);
+	
+	
+	
+}
+
+/* 
+	void myISR1(){ //used for testing purposes
+	lPrevTick = lCurrentTick;
+	lCurrentTick = xTaskGetTickCountFromISR();
+	lTickTime = lCurrentTick-lPrevTick;
+	printf("Time:\n");
+	printf("%i\n",lTickTime);
+	
+} 
+*/
+
+void rightEncoderAISR(){ //ISR for the right encoder A
+	//Counts pulses on the right wheel encoder
+	rAPulses++; 
+	
+	//Compares tick-count since last time it was here to get a measurement of the pulsetime in units of ticks
+	//It uses the inbuilt tick counter as reference 
+	rAPrevTick = rACurrentTick;
+	rACurrentTick = xTaskGetTickCountFromISR();
+	rAPulseTime = rACurrentTick-rAPrevTick*msPerTick;
+	printf("RightPulseTime: %i ms\n",rAPulseTime);
+	
+}
+
+void rightEncoderBISR(){ //ISR for the right encoder B
+	//Counts pulses on the right wheel encoder
+	rBPulses++;
+	
+	//Compares tick-count since last time it was here to get a measurement of the pulsetime in units of ticks
+	//It uses the inbuilt tick counter as reference
+	rBPrevTick = rBCurrentTick;
+	rBCurrentTick = xTaskGetTickCountFromISR();
+	rBPulseTime = rBCurrentTick-rBPrevTick*msPerTick;
+	printf("RightPulseTime B: %i ms\n",rBPulseTime);
+	
+}
+
+void leftEncoderAISR(){ //ISR for the left encoder A
+	//Counts pulses on the left wheel encoder
+	lAPulses++;
+	
+	//Compares tick-count since last time it was here to get a measurement of the pulsetime in units of ticks
+	//It uses the inbuilt tick counter as reference
+	lAPrevTick = lACurrentTick;
+	lACurrentTick = xTaskGetTickCountFromISR();
+	lAPulseTime = (lACurrentTick-lAPrevTick)*msPerTick;
+	printf("LeftPulseTime: %i ms\n",lAPulseTime);
+	
+}
+
+void leftEncoderBISR(){ //ISR for the left encoder B
+	//Counts pulses on the left wheel encoder
+	lBPulses++;
+	
+	//Compares tick-count since last time it was here to get a measurement of the pulsetime in units of ticks
+	//It uses the inbuilt tick counter as reference
+	lBPrevTick = lBCurrentTick;
+	lBCurrentTick = xTaskGetTickCountFromISR();
+	lBPulseTime = (lBCurrentTick-lBPrevTick)*msPerTick;
+	printf("LeftPulseTime B: %i ms\n",lBPulseTime);
+	
+}
+
+void resetCounter(){ //resets left and right encoders pulse counters
+	lAPulses=0;
+	rAPulses=0;
+	rBPulses=0;
+	lBPulses=0;
+	printf("Reseting pulse counters:\n LeftPulses A: #%i\n LeftPulses B: #%i\n RightPulses A: #%i\n RightPulses B: #%i\n",lAPulses,lBPulses,rAPulses,rBPulses);
+}
+/////////////////////PULSE COUNTER//////////////////////////////////////////
+int getLeftPulses(){ //returns the sum of pulses from left encoder A and B since last reset
+	printf("LeftPulses : #%i\n",(getLeftAPulses()+getLeftBPulses()));
+	return (getLeftAPulses()+getLeftBPulses());
+}
+int getLeftAPulses(){ //returns the amount of pulses from left encoder A since last reset
+	printf("LeftPulses A: #%i\n",lAPulses);
+	return lAPulses;
+}
+int getLeftBPulses(){ //returns the amount of pulses from left encoder B since last reset
+	printf("LeftPulses B: #%i\n",lBPulses);
+	return lBPulses;
+}
+int getRightPulses(){ //returns the sum of pulses from right encoder A and B since last reset
+	printf("RightPulses : #%i\n",(getRightAPulses()+getRightBPulses()));
+	return (getRightAPulses()+getRightBPulses());
+}
+int getRightAPulses(){ //returns the amount of pulses from right encoder A since last reset
+	printf("RightPulses A: #%i\n",rAPulses);
+	return rAPulses;
+}
+int getRightBPulses(){ //returns the amount of pulses from right encoder B since last reset
+	printf("RightPulses B: #%i\n",rBPulses);
+	return rBPulses;
+}
+
+/////////////////////////PULSE TIMES///////////////////////////////////////
+int getRightAPulseTime(){ //returns the latest pulse time for the right encoder A in milliseconds
+	printf("RightPulseTime A: %i ms\n",rAPulseTime);
+	return rAPulseTime;
+	
+}
+int getRightBPulseTime(){ //returns the latest pulse time for the right encoder B  in milliseconds
+	printf("RightPulseTime B: %i ms\n",rBPulseTime);
+	return rBPulseTime;
+}
+int getLeftAPulseTime(){  //returns the latest pulse time for the left encoder A in milliseconds
+	printf("LeftPulseTime A: %i ms\n",lAPulseTime);
+	return lAPulseTime;
+}
+int getLeftBPulseTime(){  //returns the latest pulse time for the left encoder B in milliseconds
+	printf("LeftPulseTime B: %i ms\n",lBPulseTime);
+	return lBPulseTime;
+}
+
+/////////////////////////CONTROL BY PULSE COUNTER///////////////////////////////
+int waitForXPulsesRL(int lPulses,int rPulses){//wait for a specified amount of pulses, returns 1 when first of two conditions of X pulses is met
+	if(getLeftPulses()>lPulses || getRightPulses()>rPulses)
+		printf("RL True\n");
+		return 1;
+	printf("RL False\n");	
+	return 0;
+}
+ 
+int waitForXPulsesL(int lPulses){//wait for a specified amount of pulses from the left wheel encoder, returns 1 if condition X pulses is met
+	if(getLeftPulses()>lPulses)
+		printf("L True\n");	
+		return 1;
+	printf("L False\n");		
+	return 0;
+}
+
+int waitForXPulsesR(int rPulses){//wait for a specified amount of pulses from the right wheel encoder, returns 1 if condition X pulses is met
+	if(getRightPulses()>rPulses)
+	    printf("R True\n");	
+		return 1;
+	printf("R False\n");	
+	return 0;
+}
---
+++ Sammanstäld kod Atmel/v.1.0/FreeRTOS/src/main.c
@@ -0,0 +1,47 @@
+#include "initializations/main_init.h"
+#include "initializations/pins_init.h"
+#include "Reglering/Reglering.h"
+#include "encoderISR.h"
+#include "motorController.h"
+#include <asf.h>
+/*
+This version Experiment 1.0 of our FreeRTOS program is ment to test the encoders, if we can count them and save the counter value 
+with appropiate values in two separate variables, one for each wheel. Clear?[]
+
+Next version (1.1) will test if we can add in P-regulation to make the wheels go at the same speed. Clear?[]
+
+Next version (1.2) will test if we can add in the distance the robot has travelled. Clear?[]
+
+Next version (1.3) will test if we can implement PWM to control the signal to the motorcontrollers. Clear?[] 
+
+*/
+int main (void)
+{
+	int ticksContinously = 0;
+	int ticksPassed = 0;
+	int ticksReference = 0;
+	sysclk_init();
+	board_init();
+	misc_init(); //pin 2 out, pin5&7 in
+	console_init();
+	//interrupt_init();	//	pin 7 and 8 as interrupt pins
+	//rtos_init(); //Creates a RTOS Task
+	encoder_init(); //also initialises pin 5 (left encoder) & 7 (right encoder) as interrupt pins
+	//delay_us(3000000);
+	//controlCenter(50);
+	drive(30, 30, 1, 1); //uses pin2
+
+	while(1){
+		printf("Test\n");
+		getRightBPulseTime();
+		getLeftAPulseTime();
+		getRightPulses();
+		getLeftPulses();
+	//ticksReference = xTaskGetTickCountFromISR();
+	}
+	/*while(ticksPassed<3000){
+		ticksPassed =  xTaskGetTickCountFromISR() - ticksReference;
+	}
+	drive(0, 0, 1, 1);
+	*/
+}
