9711231f6d7a1a8a9d0e2b5d2b654e0cc4021882
unknown <sebastian@email.com>
2018-05-04 18:03:00 +0200

    Sammasntäld kod Atmel v.1.3 Encoders funkar fint. ATT FIXA: Hjulens direktion verkar inte stämma helt med det man matar in i drive funktionen.

---
+++ Sammanstäld kod Atmel/v.1.3/FreeRTOS/src/motorController.h
@@ -0,0 +1,22 @@
+/*
+ * motorController.h
+ *
+ * Created: 2018-04-18 13:02:08
+ *  Author: filip
+ */ 
+
+
+#ifndef MOTORCONTROLLER_H_
+#define MOTORCONTROLLER_H_
+int rSpeedLatest;
+int lSpeedLatest;
+int rDirectionLatest;
+int lDirectionLatest;
+void drive(int rSpeed, int lSpeed, int rDirection, int lDirection);
+void testDriveR(int speed, int direction);
+void testDriveL(int speed, int direction);
+int getSetLSpeed(void);
+int getSetRSpeed(void);
+int getSetLDirection(void);
+int getSetRDirection(void);
+#endif /* MOTORCONTROLLER_H_ */
