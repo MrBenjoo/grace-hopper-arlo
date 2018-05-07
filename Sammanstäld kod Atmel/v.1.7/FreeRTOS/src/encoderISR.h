94774c7f410a52de64743be0c4fbdce0b1d6e97d
unknown <sebastian@email.com>
2018-05-07 16:19:16 +0200

    Sammanställd kod Atmel V1.7 Regleringskod finns nu inne men har ej testats

---
+++ Sammanstäld kod Atmel/v.1.7/FreeRTOS/src/encoderISR.h
@@ -0,0 +1,50 @@
+#ifndef ENCODERISR_H_
+#define ENCODERISR_H_
+
+
+//void myISR1(void);
+void encoder_init(void);
+void rightEncoderAISR(void);
+void leftEncoderAISR(void);
+void rightEncoderBISR(void);
+void leftEncoderBISR(void);
+void resetCounter(void);
+int getRightAPulseTime(void);
+int getLeftAPulseTime(void);
+int getRightBPulseTime(void);
+int getLeftBPulseTime(void);
+int getRightAPulses(void);
+int getLeftAPulses(void);
+int getRightBPulses(void);
+int getLeftBPulses(void);
+int getRightPulses(void);
+int getLeftPulses(void);
+int waitForXPulsesRL(int lPulses,int rPulses);
+int waitForXPulsesL(int lPulses);
+int waitForXPulsesR(int rPulses);
+
+int lAPrevTick;
+int rAPrevTick;
+int lACurrentTick;
+int rACurrentTick;
+int lBPrevTick;
+int rBPrevTick;
+int lBCurrentTick;
+int rBCurrentTick;
+int lAPulses;
+int rAPulses;
+int lAPulseTime;
+int rAPulseTime;
+int lBPulses;
+int rBPulses;
+int lBPulseTime;
+int rBPulseTime;
+int msPerTick;
+
+#endif
+/*
+ * encoderISR.h
+ *
+ * Created: 2018-04-22 23:00:56
+ *  Author: gustav
+ */ 
