4104ef78a35c415f8161f5b4061df141640a7c8b
sankiks <yohan1933@live.se>
2018-05-08 21:32:23 +0200

    reglering

--- Sammanstäld kod Atmel/v.1.5/FreeRTOS/src/tasks/task1/task1.c
+++ Sammanstäld kod Atmel/v.1.5/FreeRTOS/src/tasks/task1/task1.c
@@ -16,65 +16,27 @@
 * -------------------
 * Performs an action every xTimeT1 milliseconds
 */
+volatile uint8_t rPulse=1;
 void task1(void *pvParamters)
 {
-	//portTickType xLastWakeTime;
-	//portTickType xTimeIncrement = xTimeT1/portTICK_RATE_MS; /* set ticks (xTimeT1) in milliseconds */
-	//xLastWakeTime = xTaskGetTickCount();					/* the count of ticks since vTaskStartScheduler was called */
 	
-		//vTaskDelayUntil(&xLastWakeTime, xTimeIncrement); /* constant period, independent of execution time */
-		//vTaskDelay(xTimeT1);								 /* no constant period, dependent of execution time */ 
+	while(rPulse == getRightAPulses()){
 		
-		//int ticksContinously = 0;
+			rPulse= getRightAPulses();
 		
-	//drive(20, 20, 1, 1); //uses pin35 and pin42 vänster/höger motorstyrka, vänster/höger direktion
-
-	while(1){
-	int ticksPassed = 0;	//Do not modify	
-	int	ticksReference = 0; //Do not modify	
-	int testNbr = 1; //Do not modify	
-	int secondsPerTest = 30; //MODIFY for yout test, NOTE: take into account limits of data type if needed	
-	int speedIncrementFactor = 5; //MODIFY for your test, NOTE: Motorspeed is limited to 0-100	
-	int totalNbrTests = 20;	//MODIFY for your test, NOTE: calculation: totalNbrTests * speedIncrementFactor = should not exceed 100
-	int pulses[2][totalNbrTests]; //Do not modify
-	while(testNbr <= totalNbrTests){
-		printf("Test#%i is startning...\n",testNbr);
-		getLeftPulses();
-		getRightPulses();
-		printf("------\n");	
-		ticksReference = xTaskGetTickCount();
-		drive(speedIncrementFactor*testNbr,speedIncrementFactor*testNbr, 1, 1);
-		while(ticksPassed<secondsPerTest*1000){
-			ticksPassed =  xTaskGetTickCount() - ticksReference;
+		if (rPulse= getRightAPulses())
+		{
+			int Vl=getLeftAPulseTime();
+			int Vh=getRightAPulseTime();
+			int lineDef= Vl-Vh;
+			printf("%i",lineDef);
+			rPulse++;
 		}
-		drive(0,0,1,1);
-		pulses[1][testNbr] = getLeftPulses();
-		pulses[2][testNbr] = getRightPulses();
-		printf("Test#%i is ending...\n",testNbr);
-		printf("------\n");
-		resetCounter();	
-		testNbr++;
-	}
-	printf("All tests have been run through...\n");
-	printf("printing the results:\n");
-	printf("------\n");
-	printf("Start of results\n");
-	printf("------\n");
-	for(int j = 1; j <= 2; j++){
-		if(j==1){
-			printf("WHEEL LEFT:\n");
-		}else if(j==2){
-			printf("WHEEL RIGHT:\n");
-		}
-		for(int k = 1; k <= totalNbrTests; k++){
-			printf(" %i ",pulses[j][k]);
-		}
-	}
-	printf("------\n");
-	printf("End of results\n");
-	printf("------\n");
-	while(1){		
-	}	
+		
+		//drive(50-lineDef,  50+lineDef, 1, 1);
+		//delay_ms(5)
+		
+	
 	}
 }
 
---
+++ Sammanstäld kod Atmel/v.2.3/FreeRTOS/src/motorController.c
@@ -0,0 +1,171 @@
+/*
+ * motorController.c
+ *
+ * Created: 2018-04-18 13:01:59
+ *  Author: Filip och Carl 
+ */ 
+ #include <asf.h>
+ #include <stdio.h>
+ #include "motorController.h"
+ #include "encoderISR.h"
+ #include "../initializations/pins_init.h"
+ #include <math.h>
+ #define PI 3.14159265
+ #define PWM_35 IOPORT_CREATE_PIN(PIOC, 3) // pin 35
+ #define PWM_42 IOPORT_CREATE_PIN(PIOA, 19) // pin 42
+ uint8_t rSpeedLatest = 0;
+ uint8_t lSpeedLatest = 0;
+ int8_t rDirectionLatest = 0;
+ int8_t lDirectionLatest = 0;
+ pwm_channel_t pwm_channel_instance;
+ 
+ void drive(uint8_t lSpeed, uint8_t rSpeed, int8_t lDirection, int8_t rDirection) { //speed 0-100 in %, direction -1,1, where -1 is backwards and 1 is forward.
+
+	rSpeedLatest = rSpeed;
+	lSpeedLatest = lSpeed;
+	rDirectionLatest = rDirection;
+	lDirectionLatest = lDirection;
+	
+	testDriveR(rSpeed, -1*rDirection); // sets pwm for the right motors 
+	testDriveL(lSpeed, -1*lDirection); // sets pwm for the left motors
+
+	//int rMotorSpeed = (1500 + 7 * rSpeed * rDirection);
+	//int lMotorSpeed = (1500 + 7 * lSpeed * lDirection);
+	//
+	//ioport_set_pin_level(PIO_PB27_IDX, HIGH);
+	//delay_us(rMotorSpeed);
+	//ioport_set_pin_level(PIO_PB27_IDX, LOW);
+	//delay_us(1100);
+	//ioport_set_pin_level(PIO_PB27_IDX, HIGH);
+	//delay_us(lMotorSpeed);
+	//ioport_set_pin_level(PIO_PB27_IDX, LOW);
+	//delay_us(5250);
+}
+
+void rotate(int degree) { //degree 1 - 180 or -1 - -180
+	float radian = (degree * PI) / 180;
+	float p = (radian * 0.4 * 144) / (PI * 0.16);
+	if(radian > 0) {
+		drive(50,50,-1,1);
+	} else if(radian < 0) {
+		drive(50,50,1,-1);
+	}
+	while(waitForXPulsesRL(p,p) < 1) {
+		
+	}
+	drive(0,0,1,1);
+}
+
+int getAngle(int p1X, int p1Y, int p2X, int p2Y) {
+	// atan2 returns angle in radians, we multiply by (180/PI) to change it to degrees and add 180 to change the reference
+	return atan2(p1Y-p2Y, p1X-p2X) * (180/PI) + 180;
+}
+void angleCorrection(int rX,int rY,int cX,int cY,int u1) { // r = robotic position, c = coordinate position, u1 = angle of robot.
+	// atan2 returns angle in radians, we multiply by (180/PI) to change it to degrees and add 180 to change the reference
+	int v1 = atan2(rY-cY, rX-cX) *(180/PI) + 180; // the angle we want to have.
+	int correctionAngle = v1-u1;
+	if(correctionAngle>=0){
+		
+	}
+	else{
+		
+	}
+	//skickar så många pulser som behövs för att svänga correctionAngle många grader.
+}
+
+void testDriveR(uint8_t speed, int8_t direction) {
+	speed = Abs(speed);
+	int motorSpeed = (1500 + 7 * speed * direction);
+
+	//connect peripheral B to pin C3
+	pio_configure_pin(PWM_35, PIO_TYPE_PIO_PERIPH_B);
+
+	//enable the peripheral clock for the PWM hardware
+	pmc_enable_periph_clk(ID_PWM);
+
+	//disable the channel until it is properly configured
+	pwm_channel_disable(PWM, PWM_CHANNEL_0);
+
+	//PWM clock configuration
+	pwm_clock_t PWMDAC_clock_config =
+	{
+		.ul_clka = 1000000,
+		.ul_clkb = 0,
+		.ul_mck = sysclk_get_cpu_hz()
+	};
+
+	//apply the clock configuration
+	pwm_init(PWM, &PWMDAC_clock_config);
+
+	//sets the wave pulse properties
+	pwm_channel_instance.channel = PWM_CHANNEL_0;
+	pwm_channel_instance.ul_prescaler = PWM_CMR_CPRE_CLKA;
+	pwm_channel_instance.polarity = PWM_HIGH;
+	pwm_channel_instance.alignment = PWM_ALIGN_LEFT;
+	pwm_channel_instance.ul_period = 5250 + motorSpeed;
+	pwm_channel_instance.ul_duty = motorSpeed;
+	//apply the channel configuration
+	pwm_channel_init(PWM, &pwm_channel_instance);
+
+	//configuration is complete, so enable the channel
+	pwm_channel_enable(PWM, PWM_CHANNEL_0);
+}
+
+void testDriveL(uint8_t speed, int8_t direction) {
+	speed = Abs(speed);
+	int motorSpeed = (1500 + 7 * speed * direction);
+
+	//connect peripheral B to pin A19
+	pio_configure_pin(PWM_42, PIO_TYPE_PIO_PERIPH_B);
+
+	//enable the peripheral clock for the PWM hardware
+	pmc_enable_periph_clk(ID_PWM);
+
+	//disable the channel until it is properly configured
+	pwm_channel_disable(PWM, PWM_CHANNEL_1);
+
+	//PWM clock configuration
+	pwm_clock_t PWMDAC_clock_config =
+	{
+		.ul_clka = 1000000,
+		.ul_clkb = 0,
+		.ul_mck = sysclk_get_cpu_hz()
+	};
+
+	//apply the clock configuration
+	pwm_init(PWM, &PWMDAC_clock_config);
+
+	//sets the wave pulse properties
+	pwm_channel_instance.channel = PWM_CHANNEL_1;
+	pwm_channel_instance.ul_prescaler = PWM_CMR_CPRE_CLKA;
+	pwm_channel_instance.polarity = PWM_HIGH;
+	pwm_channel_instance.alignment = PWM_ALIGN_LEFT;
+	pwm_channel_instance.ul_period = 5250 + motorSpeed;
+	pwm_channel_instance.ul_duty = motorSpeed;
+	//apply the channel configuration
+	pwm_channel_init(PWM, &pwm_channel_instance);
+
+	//configuration is complete, so enable the channel
+	pwm_channel_enable(PWM, PWM_CHANNEL_1);
+}
+
+/*Returns the value of latest speed set for the left motor*/
+uint8_t getSetLSpeed(){
+	//printf("Set Speed for Left Motor is: %i\n%%",lSpeedLatest);
+	return lSpeedLatest;
+}
+/*Returns the value of latest speed set for the right motor*/
+uint8_t getSetRSpeed(){
+	//printf("Set Speed for Right Motor is: %i\n%%",rSpeedLatest);
+	return rSpeedLatest;
+}
+/*Returns the value of latest direction set for the left motor*/
+int8_t getSetLDirection(){
+	//printf("Set Direction for Left Motor is: %i\n",lDirectionLatest);
+	return lDirectionLatest;
+}
+/*Returns the value of latest direction set for the right motor*/
+int8_t getSetRDirection(){
+	//printf("Set Direction for Right Motor is: %i\n",rDirectionLatest);
+	return rDirectionLatest;
+}
