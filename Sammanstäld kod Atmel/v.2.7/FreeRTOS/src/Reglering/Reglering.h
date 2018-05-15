87bc208d06edcb2b03beb4cb529192f6e0456927
Unknown <benjo_953@hotmail.com>
2018-05-15 09:30:47 +0200

    Sammanställd kod Atmel V2.7 - la till I2C funktioner

---
+++ Sammanstäld kod Atmel/v.2.7/FreeRTOS/src/Reglering/Reglering.h
@@ -0,0 +1,13 @@
+/*
+ * Reglering.h
+ *
+ * Created: 2018-04-25 12:57:42
+ *  Author: yohan
+ */ 
+
+/*
+#ifndef REGLERING_H_
+#define REGLERING_H_
+void controlCenter(int distans);
+
+#endif REGLERING_H_ */ 
