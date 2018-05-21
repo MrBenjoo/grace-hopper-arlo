f4422aa2e65e736c7c9dbefe7eac134d653849e6
unknown <sebastian@email.com>
2018-05-21 04:08:40 +0200

    Merge V0.4 kvarstår att implementera Tvi och ta bort två rader kod från config samt göra färdigt alla cases och lägga till skanningslogiken

---
+++ Merge/Merge v0.4/FreeRTOS/src/motorController.h
@@ -0,0 +1,35 @@
+/*
+ * motorController.h
+ *
+ *  Author: Group Grace Hopper
+ */ 
+
+#include <asf.h>
+#include <stdio.h>
+#include <math.h>
+#include "../initializations/pins_init.h"
+
+#ifndef MOTORCONTROLLER_H_
+#define MOTORCONTROLLER_H_
+
+void drive(float velL, float velR);
+void testDriveL(float speedPulseL);
+void testDriveR(float speedPulseR);
+float getSetLeftSpeed(void);
+float getSetRightSpeed(void);
+int getSetLeftDirection(void);
+int getSetRightDirection(void);
+
+pwm_channel_t pwm_channel_instance;
+ float mcPLeftLatest;
+ float mcPRightLatest;
+ int motorSpeed;
+ 
+
+#endif /* MOTORCONTROLLER_H_ */
+
+
+
+
+
+
---
+++ Merge/Merge v0.4/FreeRTOS/src/navigation.c
@@ -0,0 +1,65 @@
+/*
+ *
+ *  Author: 
+ */ 
+
+#include "navigation.h"
+
+		
+		//Returns the difference between the actual position of the robot and a destination
+		int getDistanceToDestination(int x, int y){
+			return sqrt(pow(abs(getPositionX()-x),2) +pow(abs(getPositionY()-y),2));
+		}
+		
+		//Returns the difference between the actual position of the robot and the object it will pick up 
+		int getDistanceToObject(){
+			return sqrt(pow(abs(getPositionX()-getPickupX()),2) +pow(abs(getPositionY()-getPickupY()),2));
+		}
+
+		//Returns the difference between the actual position of the robot and the disposal box where the object will be dropped of
+		int getDistanceToDropOff(){
+			return sqrt(pow(abs(getPositionX()-getDisposalX()),2) +pow(abs(getPositionY()-getDisposalY()),2));
+		}
+
+		void driveCm(float cm,float velocity) {
+			float startline = getDisplacementAverage();
+			while((abs(getDisplacementAverage()-startline))<cm){
+				drive(velocity,velocity);
+			}	
+			drive(stop,stop);
+		}
+
+
+		//turndir: rotate left = 0, rotate right = 1
+		void rotate(float degree,float angVel,int turnDir) { //degree 1 - 180 or -1 - -180
+			float startAngle = getChangeInAngle();
+			while((abs(getChangeInAngle()-startAngle))<degree){
+				if(turnDir) //rotate right
+					drive(angVel,((-1)*angVel));
+				if(!turnDir) //rotate left
+					drive(((-1)*angVel),angVel);
+			} 
+			drive(stop,stop);
+		}
+
+		int getAngle(int p1X, int p1Y, int p2X, int p2Y) {
+			// atan2 returns angle in radians, we multiply by (180/PI) to change it to degrees and add 180 to change the reference
+			int angle = (int)((atan2(p1Y-p2Y, p1X-p2X) * (180/M_PI)) + 180);
+			if(angle == 360)
+				angle = 0;
+			return angle;
+		}
+	
+	
+		int angleCorrection(int rX,int rY,int cX,int cY,int u1) { // r = robotic position, c = coordinate position, u1 = angle of robot.
+		// atan2 returns angle in radians, we multiply by (180/PI) to change it to degrees and add 180 to change the reference
+			int v1 = atan2(rY-cY, rX-cX) *(180/M_PI) + 180; // the angle we want to have.
+			if((v1-u1) > 180)
+				return -360 + (v1-u1);
+			else if(v1-u1 < -180)
+				return 360+(v1-u1);
+			else
+				return (v1-u1);
+	//skickar så många pulser som behövs för att svänga correctionAngle många grader.
+		}
+	
