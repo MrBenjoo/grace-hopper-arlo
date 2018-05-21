a1fed87f82db8c404f90266f70bc24a061d771d9
unknown <sebastian@email.com>
2018-05-21 14:26:03 +0200

    Merge v0.4.2, kvarstår att fixa cases samt Gustavs feedback

---
+++ Merge/Merge v0.4.2/FreeRTOS/src/tasks/regulator.h
@@ -0,0 +1,25 @@
+#ifndef REGULATOR_H_
+#define REGULATOR_H_
+
+#define TASK_REG_STACK_SIZE (2048/ sizeof(portSTACK_TYPE))
+#define TASK_REG_PRIORITY (1)
+#define sampleTime 300 //Sampletime for PID-regulation [in milliseconds]
+
+#include <asf.h>
+#include <math.h>
+#include <stdio.h>
+#include "../initializations/pins_init.h"
+#include "../motorController.h"
+#include "../encoderISR.h"
+#include "../dimensions.h"
+#include "../navigation.h"
+
+void regulator(void *pvParamters);
+long getPulsesSinceStartLeft();
+long getPulsesSinceStartRight();
+float getDisplacementLeft();
+float getDisplacementRight();
+float getDisplacementAverage();
+float getChangeInAngle();
+	
+#endif /* REGULATOR_H_ */
